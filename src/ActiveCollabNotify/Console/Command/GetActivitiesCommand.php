3c49e1919407d9d144b13b9d0aa141e0a484907d
Kosta Harlan <kostajh@gmail.com>
2012-11-29 09:39:00 -0500

    Remove messages for now

--- src/ActiveCollabNotify/Console/Command/GetActivitiesCommand.php
+++ src/ActiveCollabNotify/Console/Command/GetActivitiesCommand.php
@@ -58,7 +58,7 @@ class GetActivitiesCommand extends Command
       $command = '/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier';
       $command .= ' -title \'activeCollab Notify\'';
       $command .= ' -subtitle \'' . $item['title'] . '\'';
-      $command .= ' -message \'' . $item['message'] . '\'';
+      // $command .= ' -message \'' . $item['message'] . '\'';
       $command .= ' -open \'' . $item['permalink'] . '\'';
       $command .= ' -group 139';
       $ret = exec($command);
@@ -117,7 +117,7 @@ class GetActivitiesCommand extends Command
         $feedItem['permalink'] = $item->get_permalink();
         $feedItem['title'] = $item->get_title();
         $feedItem['date'] = $item->get_date('U');
-        $feedItem['message'] = addslashes($item->get_content());
+        // $feedItem['message'] = addslashes($item->get_content());
         $newItems[$id] = $feedItem;
         $savedItems[$id] = $feedItem;
       }
